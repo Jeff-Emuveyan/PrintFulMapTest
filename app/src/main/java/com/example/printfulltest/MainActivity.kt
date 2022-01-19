bfce1be8ac7a39fe2f6936b83f0c4bb190465446
jemuveyan <jemuveyan@getcarbon.co>
2022-01-20 00:38:15 +0100

    commit

---
+++ app/src/main/java/com/example/printfulltest/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.printfulltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.printfulltest.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.lang.Exception
+import java.net.Socket
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
