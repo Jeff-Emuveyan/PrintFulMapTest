aed9da771300036194e9d3591070544c64170960
jemuveyan <jemuveyan@getcarbon.co>
2022-01-20 09:45:40 +0100

    v1

--- users/src/main/java/com/example/users/UsersFragment.kt
+++ users/src/main/java/com/example/users/UsersFragment.kt
@@ -1,13 +1,22 @@
 package com.example.users
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 
-class UsersFragment : Fragment() {
+class UsersFragment : Fragment(), OnMapReadyCallback {
 
     companion object {
         fun newInstance() = UsersFragment()
@@ -24,8 +33,58 @@ class UsersFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(UsersViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[UsersViewModel::class.java]
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val map = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        map?.getMapAsync(this)
+    }
+
+    override fun onMapReady(mMap: GoogleMap) {
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        mMap.clear() //clear old markers
+
+        val googlePlex = CameraPosition.builder()
+            .target(LatLng(37.4219999, -122.0862462))
+            .zoom(10f)
+            .bearing(0f)
+            .tilt(45f)
+            .build()
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null)
+
+        mMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(37.4219999, -122.0862462))
+                .title("Spider Man")
+                .icon(bitmapDescriptorFromVector(requireContext(), R.drawable.common_full_open_on_phone))
+        )
+
+        mMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(37.4629101, -122.2449094))
+                .title("Iron Man")
+                .snippet("His Talent : Plenty of money")
+        )
+
+        mMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(37.3092293, -122.1136845))
+                .title("Captain America")
+        )
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+        vectorDrawable!!.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+        val bitmap =
+            Bitmap.createBitmap(vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
 }
